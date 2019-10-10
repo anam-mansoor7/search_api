de003509cc5e034f3f5af3df99be5fad8d964923
Anum <anam0131@gmail.com>
2019-10-10 15:45:13 +0200

    application configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,9 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
+  config.autoload_paths += %W(#{config.root}/lib)
+
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
