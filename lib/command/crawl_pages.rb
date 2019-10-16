ac803105b2c5fc1a2a03875d66d08c221665b8a0
Anum <anam0131@gmail.com>
2019-10-17 01:46:59 +0500

    remove stop words

--- lib/command/crawl_pages.rb
+++ lib/command/crawl_pages.rb
@@ -3,6 +3,7 @@ class Command::CrawlPages
 
   BASE_URL = 'https://en.wikipedia.org'.freeze
   WORD_REGEX = /\b[a-z]{3,16}\b/.freeze
+  PAGE_TITLE_WEIGHT = 100.freeze
 
   step :get_main_page
   step :parse_linked_pages
@@ -20,19 +21,22 @@ class Command::CrawlPages
       title = row[:title]
       next unless title
       term_count_hash = Hash.new(0)
-
       puts title
       link = row[:href]
-      next_web_page = input[:main_page].link_with(:href => link).click
-      term_count_hash[title] = 100
+      next_web_page = input[:main_page].link_with(href: link).click
+      term_count_hash[title] = PAGE_TITLE_WEIGHT
       doc_text = next_web_page.css('div#bodyContent').text
+
       page = Page.find_or_create_by(link: "#{BASE_URL}#{link}", title: title)
-      doc_text.downcase.scan(WORD_REGEX) do |word|
-        word = lemmatizer.lemma(word)
-        term_count_hash[word] += 1
+
+      fetch_tokens(doc_text).each do |token|
+        token = lemmatizer.lemma(token)
+        term_count_hash[token] += 1
       end
+
       persist_term_count(page, term_count_hash)
     end
+
     Success(input)
   end
 
@@ -42,5 +46,10 @@ class Command::CrawlPages
       term = Term.find_or_create_by(name: key, page: page)
       term.update(count: value)
     end
+  end
+
+  def fetch_tokens(text)
+    stop_words = File.read(Rails.root.join('lib', 'stopwords', 'stopwords.txt'))
+    text.downcase.scan(WORD_REGEX) - stop_words.split(",")
   end
 end
